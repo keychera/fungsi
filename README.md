46ce4328675d1e33e05b141ea45ca21eb5a4c95c
keychera <kyogatama@gmail.com>
2022-04-15 08:12:25 +0700

    blog-gen 3.1 flexible HTML content

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # fungsi
+
+## Currently doing
+
+### codename: `blog-gen`
+> [Learn Haskell by building a blog generator](https://lhbg-book.link/01-about.html)
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
 module Main where
 
-import Lib
-
 main :: IO ()
-main = someFunc
+main = putStrLn myhtml
+
+myhtml = makeHtml "A title" "some content"
+
+makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+
+head_ content = "<head>" <> content <> "</head>"
+
+title_ content = "<title>" <> content <> "</title>"
+
+html_ content = "<html>" <> content <> "</html>"
+
+body_ content = "<body>" <> content <> "</body>"
+
