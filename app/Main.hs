9cd65696bd53638504d58b26f2e92a6cba800cd8
keychera <kyogatama@gmail.com>
2022-04-15 09:10:40 +0700

    blog 3.4 Safer HTML construction

--- app/Main.hs
+++ app/Main.hs
@@ -1,39 +1,50 @@
 module Main where
 
 main :: IO ()
-main = putStrLn myhtml
+main = putStrLn (render myhtml)
 
-myhtml :: String
-myhtml = 
-    makeHtml 
-        "A title"
-        (h1_ "Hello, world!" <> p_ "Let's learn about Haskell!")
-
-makeHtml :: String -> String -> String
-makeHtml title content =
+myhtml :: Html
+myhtml =
   html_
-    ( head_
-        (title_ title)
-        <> body_ content
+    "My title"
+    ( append_
+        (h1_ "Heading")
+        ( append_
+            (p_ "Paragraph #1")
+            (p_ "Paragraph #2")
+        )
     )
 
-head_ :: String -> String
-head_ = el "head"
+newtype Html = Html String
 
-title_ :: String -> String
-title_ = el "title"
+newtype Structure = Structure String
 
-html_ :: String -> String
-html_ = el "html"
+type Title = String
 
-body_ :: String -> String
-body_ = el "body"
+html_ :: Title -> Structure -> Html
+html_ title content =
+  Html
+    ( el
+        "html"
+        ( el "head" (el "title" title)
+            <> el "body" (getStructureString content)
+        )
+    )
 
-p_ :: String -> String
-p_ = el "p"
+getStructureString :: Structure -> String
+getStructureString (Structure str) = str
 
-h1_ :: String -> String
-h1_ = el "h1"
+append_ :: Structure -> Structure -> Structure
+append_ (Structure a) (Structure b) = Structure (a <> b)
+
+render :: Html -> String
+render (Html content) = content
+
+p_ :: String -> Structure
+p_ = Structure . el "p"
+
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
 
 el :: String -> String -> String
 el tag content =
